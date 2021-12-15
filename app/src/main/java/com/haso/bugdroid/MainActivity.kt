338bf6a5276b94ab10def8feb24d4091074a8635
hasofn <hasanruzweb@gmail.com>
2021-12-15 21:19:37 +0300

    next-push

--- app/src/main/java/com/haso/bugdroid/MainActivity.kt
+++ app/src/main/java/com/haso/bugdroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.haso.bugdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val randombut: Button = findViewById(R.id.gen_but)
+        val description: TextView = findViewById(R.id.des_id)
+        randombut.setOnClickListener{
+            var randomNumber = (1..6).random()
+            description.text = randomNumber.toString()
+        }
     }
 }
