8085e3a56cdd3a8289c485ac16fab44761d87254
hasofn <hasanruzweb@gmail.com>
2021-12-15 23:28:35 +0300

    .

--- app/src/main/java/com/haso/bugdroid/MainActivity.kt
+++ app/src/main/java/com/haso/bugdroid/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val description: TextView = findViewById(R.id.des_id)
         val posimage: ImageView = findViewById(R.id.poster_image)
         randombut.setOnClickListener{
-            var randomNumber = (1..9).random()
+            var randomNumber = (1..7).random()
             when(randomNumber){
                 1 -> {
                     posimage.setImageResource(R.drawable.kotlin01)
@@ -31,23 +31,22 @@ class MainActivity : AppCompatActivity() {
                     description.text = "As Kotlin compiles to JVM bytecode or JavaScript, Java and JS developers will be most tempted to learn this programming language. Added to this, the programmers who use a garbage collected runtime will also find Kotlin programming language interesting."
                 }
                 4 -> {
-
+                    posimage.setImageResource(R.drawable.kotlin04)
                     description.text = "The feature scope and necessity of a class is always argued by the programming language designers. A typical data class in Java has tons of boilerplate code one needs to skip while finding out the real use of that class. However, in Kotlin, you can write the equivalent of the same Java code in a very simple manner and save tons of typing and mental effort. It’s one of the best features of Kotin programming language."
                 }
                 5 -> {
-
+                    posimage.setImageResource(R.drawable.kotlin05)
                     description.text = "The defaulted parameters in Kotlin are pretty handy when you pass the arguments by name, instead of index. Their advantage is seen when there’s a function with tons of optional parameters."
                 }
                 6 -> {
-
+                    posimage.setImageResource(R.drawable.kotlin06)
                     description.text = "The standard Kotlin library doesn’t have garbage, it’s tight and small. It has mostly focused extensions to the Java standard library. Many of its functions are inline-only that just become inline code. Kotlin has many optimizations which, specifically, help Android development."
                 }
                 7 -> {
-
+                    posimage.setImageResource(R.drawable.kotlin07)
                     description.text = "Kotlin programming language’s type system aims at eliminating the perils of null references from code, which is often called The Billion Dollar Mistake. For example, in Java, accessing a member of null reference results in a null reference exception. Kotlin doesn’t compile code that assigns or returns a null. This is seen as one of the most important features of Kotlin."
                 }
-                8 -> {description.text = "Thanks to the extension functions in Kotlin, you can add methods to classes without making changes to their source code. Similar to Scala’s implicit methods, you can add methods on a per-user basis to classes. While extension functions are often controversial, every now and then it’s very useful."
-                9 -> description.text = "Unlike Java, which needs you to write everything, Kotlin compiler can understand from the code and write the remaining code, for example, it can infer types in variable declarations. This increases productivity and saves time. Kotlin makes Android development more fun. If you search the web, you’ll find tons of ways in which Java’s pain points are solved by Kotlin and how features of Kotlin aim to make Android development more fun. You can use it write more expressive and effective code with fewer bugs."
+
 
             }
         }
